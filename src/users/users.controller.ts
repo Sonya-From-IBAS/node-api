79ee2333146a65ac93a6845fd74dd11cbdfa027d
Leonid <Anevskyi2018@yandex.ru>
2024-02-27 12:45:11 +0400

    added dependency injection

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,23 +1,26 @@
-import { NextFunction, Response, Request } from "express";
-import { BaseController } from "../common/base.controller";
-import { LoggerService } from "../logger/logger.service";
-import { HTTPError } from "../errors/http-error";
-
-export class UserController extends BaseController{
-    constructor(logger: LoggerService) {
-        super(logger);
+import { NextFunction, Response, Request } from 'express';
+import { BaseController } from '../common/base.controller';
+import { HTTPError } from '../errors/http-error';
+import { injectable, inject } from 'inversify';
+import { ILogger } from '../logger/logger.interface';
+import { TYPES } from '../types';
+import 'reflect-metadata';
+import { IUserController } from './users.controller.interface';
+@injectable()
+export class UserController extends BaseController implements IUserController {
+    constructor(@inject(TYPES.ILogger) private loggerSerivce: ILogger) {
+        super(loggerSerivce);
         this.bindRoutes([
-            {path: '/register', method: 'post', func: this.register},
-            {path: '/login', method: 'post', func: this.login}
-        ])
+            { path: '/register', method: 'post', func: this.register },
+            { path: '/login', method: 'post', func: this.login },
+        ]);
     }
 
     login(req: Request, res: Response, next: NextFunction) {
         next(new HTTPError(401, 'Ошибка авторизации', 'login'));
     }
-    
+
     register(req: Request, res: Response, next: NextFunction) {
         this.ok(res, 'register');
-        
     }
 }
